37513a632209304f72f3d49d244bbf47f983146a
Chakkrit Chueangyang <him.chakkrit@hotmail.com>
2021-05-07 14:35:24 +0700

    Feat: move App to src

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DefaultScreen from './DefaultScreen';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <DefaultScreen />
+    </React.Fragment>
+  );
+};
+
+export default App;
