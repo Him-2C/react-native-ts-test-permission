b60565fd07a9f55f1091836d8431492419dd5a7b
Chakkrit Chueangyang <him.chakkrit@hotmail.com>
2021-05-08 00:03:52 +0700

    Feat: clean up

--- src/utils/AppPhotos.ts
+++ src/utils/AppPhotos.ts
@@ -1,5 +1,4 @@
 import CameraRoll from '@react-native-community/cameraroll';
-import {AppPermission} from './AppPermission';
 
 export declare namespace AppPhotosType {
   type PhotosPage = CameraRoll.PhotoIdentifiersPage;
@@ -7,7 +6,7 @@ export declare namespace AppPhotosType {
 }
 
 export class AppPhotos {
-  private static emptyResponse: AppPhotosType.PhotosPage = {
+  private static _EmptyPage: AppPhotosType.PhotosPage = {
     edges: [],
     page_info: {
       has_next_page: false,
@@ -19,18 +18,12 @@ export class AppPhotos {
   static async getPhotos(
     params: CameraRoll.GetPhotosParams,
   ): Promise<AppPhotosType.PhotosPage> {
-    const isCanAcess = await AppPermission.reqPermission('photos');
-
-    if (!isCanAcess) {
-      return this.emptyResponse;
-    }
-
     try {
       const res = await CameraRoll.getPhotos(params);
 
       return res;
     } catch (error) {
-      return this.emptyResponse;
+      return this._EmptyPage;
     }
   }
 }
